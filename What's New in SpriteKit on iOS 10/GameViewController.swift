ea251b80e1d0f6c1e94953c5b1e4972fb1be897c
tnantoka <tnantoka@bornneet.com>
2016-09-21 23:33:19 +0900

    Delete sks files

--- What's New in SpriteKit on iOS 10/GameViewController.swift
+++ What's New in SpriteKit on iOS 10/GameViewController.swift
@@ -16,14 +16,9 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
